d2948dcda36209f85dc7d65c315f38de27845f37
samad <abdessamad.bouchelga@gmail.com>
2023-04-24 08:12:22 -0700

    width

---
+++ gwidth.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * gwidth - calculates width for printing
+ * @format: formatted string in which to print the arguments
+ * @ag: list of arguments to be printed
+ * @list: list of arguments
+ *
+ * Return: width
+ */
+int gwidth(const char *format, int *ag, va_list list)
+{
+	int k;
+	int width = 0;
+
+	for (k = *ag + 1; format[k] != '\0'; k++)
+	{
+		if (is_digit(format[k]))
+		{
+			width *= 10;
+			width += format[k] - '0';
+		}
+		else if (format[k] == '*')
+		{
+			k++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*ag = k - 1;
+
+	return (width);
+}
