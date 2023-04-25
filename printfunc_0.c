c5dac044c2f679bb8f490e8cb99926564bca9d0e
saraiin <saraennaciri96@gmail.com>
2023-04-24 17:45:19 -0700

    add function"

--- printfunc_0.c
+++ printfunc_0.c
@@ -156,4 +156,24 @@ int pr_binary(va_list types, char buffer[],
 	}
 	return (ctr);
 }
-
+/**
+ * pr_percent - Prints a percent sign
+ * @types: Liste of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int pr_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
